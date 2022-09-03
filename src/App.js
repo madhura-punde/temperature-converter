cf57ea67aa36c67da496db6689144947db21a90f
Madhura Punde <madhurapunde95@gmail.com>
2022-09-03 18:48:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [cel, setcel] = useState('0')
+  const [Far, SetFar] = useState('0')
+
+  const HandleUserInput=(e)=>{
+    console.log('In Cel',e.target.value)
+    setcel((e.target.value.toString()))
+    console.log(cel)
+
+    // if(cel){
+      const res = ((cel*(9/5))+32)
+      SetFar(res)
+      console.log(res)
+
+    // }
+    // (C × 9/5) + 32 = F
+  }
+  const HandleUserInput1=(e)=>{
+    console.log('In Far')
+    SetFar((e.target.value.toString()))
+    const res = (Far-32)/(9/5)
+    console.log(res)
+      setcel(res)
+      console.log(Far)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card mx-auto" style={{"width": "20rem"}}>
+       <div className="card-body">
+                    <h5 className="card-title">Temperature Converter</h5>
+                    {/* <p className="card-text" style={{backgroundColor:'#ff8000'}}>Formula</p> */}
+                    <p className="card-text">Formula</p>
+                    <p>(19.44&deg;C * 9/5) + 32 = 67&deg;F</p>
+                   
+                    <input placeholder='Celcius' value={cel} 
+                      onChange={(e)=>{
+                        console.log(e.target)
+                        HandleUserInput(e)
+                                     }}/>
+
+                      <fieldset disabled>
+                       <select class="custom-select ">
+                       <option selected>Celcius</option>
+                       </select>
+                       </fieldset>
+                   
+                  
+                    <input placeholder='Far' value={Far} 
+                      onChange={(e)=>{
+                        HandleUserInput1(e)}}
+                       style={{'marginTop': '20px'}} />
+
+                      <fieldset disabled>
+                       <select class="custom-select mr-sm-2">
+                       <option selected>Fahrenheit</option>
+                       </select>
+                       </fieldset>
+                    
+        </div>
+    </div>
     </div>
   );
 }
